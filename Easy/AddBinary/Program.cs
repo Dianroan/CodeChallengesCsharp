58f42e6d7de2a5d0c4e846defed7dbec906caea0
Diego Go <143672544+Dianroan@users.noreply.github.com>
2023-11-26 15:05:27 -0600

    other easy challenge

---
+++ Easy/AddBinary/Program.cs
@@ -0,0 +1,57 @@
+﻿
+// Dianroan - The fault in our programs
+// Code challenge: AddBinary
+
+using System;
+using static System.Console;
+
+public class Program {
+    // Function to add binary numbers
+    static public string AddBinary(string a, string b) {
+        // Initialize carry
+        int carry = 0;
+
+        // Initialize pointers for a and b
+        int i = a.Length - 1; 
+        int j = b.Length - 1; 
+
+        // Initialize result string
+        string result = "";  
+
+        // Loop through each digit of a and b
+        while (i >= 0 || j >= 0 || carry > 0)
+        {
+            // Calculate sum of digits at current position
+            int sum = carry; 
+            if (i >= 0)
+            {
+                sum += a[i] - '0'; // Convert char to int
+                i--; 
+            }
+            if (j >= 0)
+            {
+                sum += b[j] - '0'; // Convert char to int
+                j--; 
+            }
+
+            // Append the sum modulo 2 to the result string
+            result = (sum % 2) + result; 
+
+            // Update carry for next iteration
+            carry = sum / 2; 
+        }
+
+        // Return the result string
+        return result;
+    }
+
+    public static void Main(string[] args) {
+        // Test the function with example inputs
+        string a = "11";
+        string b = "1";
+        string result = AddBinary(a, b);
+        WriteLine($"The sum of {a} and {b} is {result}");
+    }
+}
+//
+//This code defines a function called `AddBinary` that takes two strings representing binary numbers. It calculates the sum of these binary numbers and returns the result as a string. The function uses a while loop to iterate through each digit of the binary numbers. It calculates the sum of the digits at each position and updates the carry for the next iteration. The result string is built by appending the sum modulo 2 at each position. The `Main` function tests the `AddBinary` function with example inputs..</s>
---
+++ Easy/Sqrt(x)/Program.cs
@@ -0,0 +1,43 @@
+﻿
+// Dianroan - The fault in our programs
+// Code challenge: Sqrt(x)
+
+using System;
+using static System.Console;
+
+public class Program {
+    // Function to calculate the square root of a given number
+    static public int MySqrt(int x) {
+        // Initialize the square root variable
+        double sqrt = 0;
+
+        // Iterate through each number up to the given number
+        for (double i = 0; i <= x; i++){
+            // Check if the square of the current number is equal to the given number
+            if((i * i) == x){
+                // If it is, set the square root to the current number
+                sqrt = i;
+                break;
+            }
+            // Check if the square of the is greater than the given number
+            else if((i * i) > x){
+                // If it is, set the square root to the previous number
+                sqrt = i - 1;
+                break;
+            }
+        }
+
+        // Convert the square root to an integer and return it
+        return Convert.ToInt32(sqrt);
+    }
+
+    public static void Main(string[] args) {
+        // Test the function with some example inputs
+        WriteLine(MySqrt(4)); // Output: 2
+        WriteLine(MySqrt(9)); // Output: 3
+        WriteLine(MySqrt(16)); // Output: 4
+        WriteLine(MySqrt(25)); // Output: 5
+    }
+}
+//
+//This code defines a function called `MySqrt` that calculates the square root of a given number. The function iterates through each number up to the given number and checks if the square of the current number is equal to the given number.
