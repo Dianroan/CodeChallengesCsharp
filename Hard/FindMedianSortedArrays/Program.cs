3e111ea5c61b98e4190933a4ae784791aac3c622
Diego Go <143672544+Dianroan@users.noreply.github.com>
2023-11-26 15:28:26 -0600

    Medium and harde challenge

---
+++ Hard/FindMedianSortedArrays/Program.cs
@@ -0,0 +1,45 @@
+﻿
+
+// Dianroan - The fault in our programs
+// Code challenge: FindMedianSortedArrays
+
+using System;
+using static System.Console;
+
+public class Program {
+    // Function to find the median of two sorted arrays
+    static public double FindMedianSortedArrays(int[] nums1, int[] nums2) {
+        double median = 0;
+        double[] array = new double[nums1.Length + nums2.Length];
+        for (int i = 0; i < nums1.Length; i++){
+            array[i] = nums1[i];
+        }
+        for (int i = 0; i < nums2.Length; i++){
+            array[i + nums1.Length] = nums2[i];
+        }
+        Array.Sort(array);
+        if(array.Length % 2 == 0){
+            median = ((array[(array.Length/2) - 1] + array[(array.Length/2)])) / 2;
+        }
+        else{
+            median = array[array.Length/2];
+        }
+        return median; 
+    }
+
+    // Main function to test the solution
+    public static void Main(string[] args) {
+        int[] nums1 = {1, 3, 4};
+        int[] nums2 = {2};
+        double median = FindMedianSortedArrays(nums1, nums2);
+        WriteLine($"The median of the two arrays is: {median}");
+    }
+}
+// have a
+//In this code, we function called `FindMedianSortedArrays` that takes two integer arrays as input. The function combines the two arrays into a new array called `array`. Then, it sorts the `array` in ascending order.
+//
+//After sorting, the function checks if the length of the `array` is even or odd. If it's even, the median is calculated as the average of the two middle numbers. If it's odd, the median is the middle number.
+//
+//Finally, the function returns the calculated median.
+//
+//The `Main` function is used to test the solution. It creates two integer arrays called `nums1` and `nums2`, and then calls the `FindMedianSortedArrays` function with these two arrays as arguments. The result is printed to the console..</s>
---
+++ Medium/TwoSumII/Program.cs
@@ -0,0 +1,41 @@
+﻿
+// Dianroan - The fault in our programs
+// Code challenge: Two Sum II
+
+using System;
+using static System.Console;
+
+public class Program {
+    // Function to find the two numbers that add up to the target
+    static public int[] TwoSum(int[] numbers, int target) {
+        int[] answerArray = new int[2];
+        int comparator;
+
+        // Iterate through the array
+        for (int arrayIndex = 0; arrayIndex < numbers.Length; arrayIndex++) {
+            comparator = target - numbers[arrayIndex];
+
+            // Iterate through the rest of the array to find the second number
+            for (int arrayIndexTwo = arrayIndex + 1; arrayIndexTwo < numbers.Length; arrayIndexTwo++) {
+                if (comparator == numbers[arrayIndexTwo]) {
+                    answerArray[0] = arrayIndex + 1;
+                    answerArray[1] = arrayIndexTwo + 1;
+                    break;
+                }
+            }
+        }
+        return answerArray;
+    }
+
+    public static void Main(string[] args) {
+        // Example usage
+        int[] numbers = { 2, 7, 11, 15 };
+        int target = 9;
+        int[] result = TwoSum(numbers, target);
+        WriteLine($"The two numbers that add up to {target} are at indices {result[0] - 1} and {result[1] - 1}.");
+    }
+}
+//
+//In this code, the `TwoSum` function iterates through the array and calculates the difference between the target and the current number. It then iterates through the rest of the array to find a number that equals this difference. The indices of the two numbers are stored in the `answerArray returned.
+//
+//The `Main` function demonstrates the usage of the `TwoSum` function with an example array and target..</s>
